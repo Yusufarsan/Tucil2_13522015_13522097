c98f7f41544fe4e2f69f2cc7d62922a842d704d9
Yusufarsan <sandiyusufardian@gmail.com>
2024-03-17 19:55:00 +0700

    feat: Added brute force method

--- src/main.py
+++ src/main.py
@@ -1,2 +1,39 @@
 # ini file untuk memanggil semua fungsi dari file lain
 # file ini sebagai program utama yang akan dijalankan
+
+from src.masukkan import get_input_terminal, get_input_file
+from src.brute_force import brute_force_process
+
+print('''Welcome to Bezier Curve Visualization Program
+made by:
+13522015 - Yusuf Ardian Sandi
+13522097 - Ellijiah DarrelShane
+      
+Choose the input method:
+1. Terminal
+2. File
+''')
+
+# Get the input method from the user
+input_method = int(input("Enter the input method: "))
+print()
+
+# Get the control points and iterate number from the user input
+if input_method == 1:
+    control_points, iterate_number = get_input_terminal()
+else:
+    control_points, iterate_number = get_input_file()
+
+# Get process method from the user
+print('''Choose the process method:
+1. Brute Force
+2. Divide and Conquer
+''')
+process_method = int(input("Enter the process method: "))
+print()
+
+# Process the brute force method
+if process_method == 1:
+    brute_force_process(control_points, iterate_number)
+else:
+    print("Error: Divide and Conquer process method is not available")
--- src/masukkan.py
+++ src/masukkan.py
@@ -1,1 +1,46 @@
 # ini file untuk nge handle inputan
+
+from typing import List
+
+def get_control_points() -> List[tuple[int, int]]:
+    '''Get the control points from the user input'''
+    control_points = []
+    n = int(input("Enter the number of control points: "))
+    print()
+    for i in range(n):
+        x = int(input(f"Enter x{i+1}: "))
+        y = int(input(f"Enter y{i+1}: "))
+        print()
+        control_points.append((x, y))
+    return control_points
+
+def get_iterate_number() -> int:
+    '''Get the iterate number from the user input'''
+    iterate_number = int(input("Enter the iterate number: "))
+    print()
+    return iterate_number
+
+def get_input_terminal() -> tuple[List[tuple[int, int]], int]:
+    '''Get the control points and iterate number from the user input'''
+    control_points = get_control_points()
+    iterate_number = get_iterate_number()
+    return control_points, iterate_number
+
+def get_input_file() -> tuple[List[tuple[int, int]], int]:
+    '''Get the control points and iterate number from the input file'''
+    control_points = []
+    iterate_number = 0
+    filename = input("Enter the file name without extension: ")
+    with open(f"../test/{filename}.txt", "r") as file:
+        n = int(file.readline())
+        iterate_number = int(file.readline())
+        for i in range(n):
+            x, y = map(int, file.readline().split())
+            control_points.append((x, y))
+    return control_points, iterate_number
+# File txt example:
+# 3 <- number of control points
+# 100 <- iterate number
+# 0 0 <- control point 1
+# 5 10 <- control point 2
+# 10 5 <- control point 3
